290040a36bef4846bfdc62e58e6bd48e752a4179
jevian ma(马作伟) <jevian_ma@126.com>
2018-09-22 17:49:47 +0800

    first commit

---
+++ src/ios/NativeCode.m
@@ -0,0 +1,28 @@
+/********* NativeCode.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface NativeCode : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation NativeCode
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
